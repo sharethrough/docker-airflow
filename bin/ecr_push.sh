ca54dda746516f661903d7c4b5d5587fec630096
Zachery O'Dell <zdell@sharethrough.com>
2019-07-24 13:04:33 -0700

    Remove configure statements

--- bin/ecr_push.sh
+++ bin/ecr_push.sh
@@ -4,10 +4,6 @@ REPO=airflow-base
 TARGET_IMAGE="119933218031.dkr.ecr.us-east-1.amazonaws.com/${REPO}"
 TARGET_IMAGE_LATEST="${TARGET_IMAGE}:latest"
 
-aws configure set default.region us-east-1
-aws configure set aws_access_key_id ${AWS_ACCESS_KEY_ID}
-aws configure set aws_secret_access_key ${AWS_SECRET_ACCESS_KEY}
-
 echo "Authenticating with ecr"
 eval $(aws ecr get-login --no-include-email --region us-east-1)
 
