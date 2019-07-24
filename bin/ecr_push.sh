64d118c9c3eb6cf64e0a8a0e2019a1182db47dd0
Zachery O'Dell <zdell@sharethrough.com>
2019-07-24 13:53:22 -0700

    Add more debug statements

--- bin/ecr_push.sh
+++ bin/ecr_push.sh
@@ -4,8 +4,13 @@ REPO=airflow-base
 TARGET_IMAGE="119933218031.dkr.ecr.us-east-1.amazonaws.com/${REPO}"
 TARGET_IMAGE_LATEST="${TARGET_IMAGE}:latest"
 
+echo "Setting region"
 aws configure set default.region us-east-1
+
+echo "Setting access key"
 aws configure set aws_access_key_id ${AWS_ACCESS_KEY_ID}
+
+echo "Setting secret key"
 aws configure set aws_secret_access_key ${AWS_SECRET_ACCESS_KEY}
 
 echo "Authenticating with ecr"
