421113a82d80fccd0043e153ddcced5fc6c10255
Zachery O'Dell <zdell@sharethrough.com>
2019-07-24 09:42:49 -0700

    Add default region

--- bin/ecr_push.sh
+++ bin/ecr_push.sh
@@ -4,8 +4,7 @@ REPO=airflow-base
 TARGET_IMAGE="119933218031.dkr.ecr.us-east-1.amazonaws.com/${REPO}"
 TARGET_IMAGE_LATEST="${TARGET_IMAGE}:latest"
 
-
-echo "AWS Creds: ${AWS_ACCESS_KEY_ID} ${AWS_SECRET_ACCESS_KEY}"
+aws configure set default.region us-east-1
 
 echo "Authenticating with ecr"
 eval $(aws ecr get-login --no-include-email --region us-east-1)
