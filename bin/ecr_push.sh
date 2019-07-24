6e3c46af85ef0d3dea8b8cd45e16d3a39b74a576
Zachery O'Dell <zdell@sharethrough.com>
2019-07-24 00:37:33 -0700

    More travis adjustments

--- bin/ecr_push.sh
+++ bin/ecr_push.sh
@@ -4,9 +4,6 @@ REPO=airflow-base
 TARGET_IMAGE="119933218031.dkr.ecr.us-east-1.amazonaws.com/${REPO}"
 TARGET_IMAGE_LATEST="${TARGET_IMAGE}:latest"
 
-TIMESTAMP=$(date '+%Y%m%d%H%M%S')
-VERSION="${TIMESTAMP}-${TRAVIS_COMMIT}"
-TARGET_IMAGE_VERSIONED="${TARGET_IMAGE}:${VERSION}"
 
 echo "Authenticating with ecr"
 eval $(aws ecr get-login --no-include-email --region us-east-1)
@@ -15,6 +12,3 @@ echo "Pushing built image to ecr"
 
 docker tag ${REPO} ${TARGET_IMAGE_LATEST}
 docker push ${TARGET_IMAGE_LATEST}
-
-docker tag ${REPO} ${TARGET_IMAGE_VERSIONED}
-docker push ${TARGET_IMAGE_VERSIONED}
