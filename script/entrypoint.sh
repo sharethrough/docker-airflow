b8af4960a2437f0672433f8a84af038b785aa506
Zachery O'Dell <zdell@sharethrough.com>
2019-08-22 12:24:54 -0700

    Lower flask version

--- script/entrypoint.sh
+++ script/entrypoint.sh
@@ -2,9 +2,12 @@
 
 echo "Beginning entrypoint script"
 
-IP_ADDR=$(curl -s http://169.254.170.2/v2/metadata | jq -r .Containers[0].Networks[0].IPv4Addresses[0])
-LOCAL_HOSTNAME=$(curl http://169.254.169.254/latest/meta-data/local-hostname)
-IP_ADDR_DASH="${IP_ADDR//./-}"
+if [[ $ENVIRONMENT = "production" ]]; then
+        IP_ADDR=$(curl -s http://169.254.170.2/v2/metadata | jq -r .Containers[0].Networks[0].IPv4Addresses[0])
+        LOCAL_HOSTNAME=$(curl http://169.254.169.254/latest/meta-data/local-hostname)
+        IP_ADDR_DASH="${IP_ADDR//./-}"
+fi
+
 
 export local_hostname=${LOCAL_HOSTNAME}
 export celery_worker="celery@ip-${IP_ADDR_DASH}.ec2.internal"
@@ -19,107 +22,96 @@ echo "Queue name: ${queue_name}"
 
 #Parse aws secrets
 for var in "${!AWS_SECRET_@}"; do
-    echo "Processing ${var}"
-    ENV_NAME=$( echo ${var} | cut -d '_' -f3- )
-    ENV_VALUE=$( echo ${!var} | jq -r .SECRET_VALUE )
-    echo "Setting ${ENV_NAME}"
-    eval export $ENV_NAME=\$ENV_VALUE
+        echo "Processing ${var}"
+        ENV_NAME=$( echo ${var} | cut -d '_' -f3- )
+        ENV_VALUE=$( echo ${!var} | jq -r .SECRET_VALUE )
+        echo "Setting ${ENV_NAME}"
+        eval export $ENV_NAME=\$ENV_VALUE
 done
 
 
 TRY_LOOP="20"
 
 wait_for_port() {
-  local name="$1" host="$2" port="$3"
-  local j=0
+        local name="$1" host="$2" port="$3"
+        local j=0
 
-  echo "Attempting to connect to $host with port $port" 
+        echo "Attempting to connect to $host with port $port" 
 
-  while ! nc -z "$host" "$port" >/dev/null 2>&1 < /dev/null; do
-    j=$((j+1))
-    if [ $j -ge $TRY_LOOP ]; then
-      echo >&2 "$(date) - $host:$port still not reachable, giving up"
-      exit 1
-    fi
-    echo "$(date) - waiting for $name... $j/$TRY_LOOP"
-    sleep 5
-  done
+        while ! nc -z "$host" "$port" >/dev/null 2>&1 < /dev/null; do
+                j=$((j+1))
+                if [ $j -ge $TRY_LOOP ]; then
+                        echo >&2 "$(date) - $host:$port still not reachable, giving up"
+                        exit 1
+                fi
+                echo "$(date) - waiting for $name... $j/$TRY_LOOP"
+                sleep 5
+        done
 
-  echo "Successfully connected to $host with port $port" 
+        echo "Successfully connected to $host with port $port" 
 }
 
 if [[ $EXECUTOR_TYPE = "CeleryExecutor" ]]; then
-  wait_for_port "Mysql" "$MYSQL_HOST" "$MYSQL_PORT"
-  wait_for_port "Redis" "$REDIS_HOST" "$REDIS_PORT"
+        wait_for_port "Mysql" "$MYSQL_HOST" "$MYSQL_PORT"
+        wait_for_port "Redis" "$REDIS_HOST" "$REDIS_PORT"
 fi
 
 if [[ $EXECUTOR_TYPE = "LocalExecutor" ]]; then
-  wait_for_port "Mysql" "$MYSQL_HOST" "$MYSQL_PORT"
+        wait_for_port "Mysql" "$MYSQL_HOST" "$MYSQL_PORT"
 fi
 
 echo "All ports checked successfully"
 
 handle_worker_term_signal() {
-  echo "Worker termination signal received"
-  echo "Broker url: ${broker_url}"
-  echo "Celery url: ${celery_worker}"
-  echo "Queue name: ${queue_name}"
+        echo "Worker termination signal received"
+        echo "Broker url: ${broker_url}"
+        echo "Celery url: ${celery_worker}"
+        echo "Queue name: ${queue_name}"
 
-  echo "Cancelling queue consumer"
-  # Try to cancel consuming from queue
-  celery -b $broker_url -d $celery_worker control cancel_consumer $queue_name
-  echo "Finished cancelling queue consumer"
+        echo "Cancelling queue consumer"
+        # Try to cancel consuming from queue
+        celery -b $broker_url -d $celery_worker control cancel_consumer $queue_name
+        echo "Finished cancelling queue consumer"
 
-  while (( $(celery -b $broker_url inspect active --json | python -c "import sys, json; print (len(json.load(sys.stdin)['$celery_worker']))") > 0 )); do
-    echo "Sleeping..."
-    sleep 60
-  done
+        while (( $(celery -b $broker_url inspect active --json | python -c "import sys, json; print (len(json.load(sys.stdin)['$celery_worker']))") > 0 )); do
+                echo "Sleeping..."
+                sleep 60
+        done
 
-  echo "Killing worker..."
-  kill $pid
-  exit 0
+        echo "Killing worker..."
+        kill $pid
+        exit 0
 }
 
 handle_general_term_signal() {
-  echo "Killing non worker process..."
-  kill $pid
-  exit 0
+        echo "Killing non worker process..."
+        kill $pid
+        exit 0
 }
 
 case "$1" in
-  webserver)
-    airflow initdb
+        webserver)
+                airflow initdb
 
-    trap handle_general_term_signal SIGTERM
-    exec airflow "$@" & pid="$!"
-    wait $pid
-    ;;
-  worker)
-    # To give the webserver time to run initdb.
-    sleep 10
-    trap handle_worker_term_signal SIGTERM
-    exec airflow worker & pid="$!"
-    wait $pid
-    ;;
-  scheduler)
-    sleep 10
-    trap handle_general_term_signal SIGTERM
-    exec airflow "$@" & pid="$!"
-    wait $pid
-    ;;
-  flower)
-    sleep 10
-    trap handle_general_term_signal SIGTERM
-    exec airflow "$@" & pid="$!"
-    wait $pid
-    ;;
-  version)
-    trap handle_general_term_signal SIGTERM
-    exec airflow "$@" & pid="$!"
-    wait $pid
-    ;;
-  *)
-    # The command is something like bash, not an airflow subcommand. Just run it in the right environment.
-    exec "$@"
-    ;;
+                trap handle_general_term_signal SIGTERM
+                exec airflow "$@" & pid="$!"
+                wait $pid
+                ;;
+        worker)
+                # To give the webserver time to run initdb.
+                sleep 10
+                trap handle_worker_term_signal SIGTERM
+                exec airflow worker & pid="$!"
+                wait $pid
+                ;;
+        scheduler|flower|version)
+                sleep 10
+                trap handle_general_term_signal SIGTERM
+                exec airflow "$@" & pid="$!"
+                wait $pid
+                ;;
+        *)
+                # The command is something like bash, not an airflow subcommand. Just run it in the right environment.
+                exec "$@"
+                ;;
 esac
